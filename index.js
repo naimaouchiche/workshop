fcbbe89d16d20b88ff33f031be2547cddfba23f2
FIRST_AOUCHICHE LAST_NAIM <naim.aouchiche@sncf.fr>
2022-05-09 14:29:30 +0200

    add fichier

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+console.log(chalk.blue('Hello Lisa!'));
+console.log(chalk.red('Hello bill!'));
+console.log(chalk.yellow('Hello Martin!'));
+console.log(chalk.white('Hello bart!'));
